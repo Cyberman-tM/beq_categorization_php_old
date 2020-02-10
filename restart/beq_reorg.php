9332f28d4b4f79cbc4c22380d015f3c15436eb0c
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-02-10 21:24:00 +0100

    Create beq_reorg.php

---
+++ restart/beq_reorg.php
@@ -0,0 +1,8 @@
+<?php
+//Fehler sollen angezeigt werden, ich bin noch beim entwickeln
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "No syntax errors, apparently.\n\n";
+
+?php>
